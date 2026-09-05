ea20748d91e915221fd846fbf28ad6dc23df5987
Delie Ageha Sonoda <37294965+Deliternity@users.noreply.github.com>
2026-09-05 21:17:39 +0000

    Update pd_process.c

--- Source/Lib/Codec/pd_process.c
+++ Source/Lib/Codec/pd_process.c
@@ -679,6 +679,18 @@ static void calc_mini_gop_activity(
 
     const bool cond3 = MIN(sub_layer_mv_in_out_count1, sub_layer_mv_in_out_count2) > 40 && MAX(sub_layer_mv_in_out_count1, sub_layer_mv_in_out_count2) > 55;
 
+    // aka: Apparently cplx is incremented when SAD is higher than given threshold
+    // aka: in_out_count is whether the vectors point towards the centre and NOT whether vectors point outside the screen
+    // aka:              it technically can detect zoom, but in the case of anime, it's very unreliable due to the random lineart vectors.
+    // aka: TO BE IMPLEMENTED
+    // aka: Break 32 into 2 16 when top_layer_dist, sub_layer_dist0, and sub_layer_dist1 all bigger than LOW_DIST_TH
+    // aka:                    OR either sub_layer_dist0 or sub_layer_dist1 bigger than HIGH_DIST_TH
+    // aka: CONSIDER further breaking 16 into 2 8 if certain TH is reached
+    fprintf(stderr, "\n%u / perc_active %u %u %u / cond1 %u\n", enc_ctx->intra_period_position, top_layer_perc_active, sub_layer0_perc_active, sub_layer1_perc_active, cond1);
+    fprintf(stderr, "%u / dist %llu %llu %llu / perc_cplx %u %u %u / cond2 %u\n", enc_ctx->intra_period_position, top_layer_dist, sub_layer_dist0, sub_layer_dist1,
+                                                                                                                  top_layer_perc_cplx, sub_layer0_perc_cplx, sub_layer1_perc_cplx, cond2);
+    fprintf(stderr, "%u / mv_in_out_count %d %d %d / cond3 %u\n", enc_ctx->intra_period_position, top_layer_mv_in_out_count, sub_layer_mv_in_out_count1, sub_layer_mv_in_out_count2, cond3);
+
     if (cond1 && (cond2 || cond3)) {
 
         ctx->mini_gop_activity_array[top_layer_idx] = TRUE;
